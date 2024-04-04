88ccfe46f8991ff803e0d26fdebc0914d74e28b4
nabishec <nabishechkaaa@gmail.com>
2024-04-04 15:56:35 +0300

    yandexalg homerwork1 task C

--- trash/linetrash.py
+++ trash/linetrash.py
@@ -1,18 +1,1 @@
-def group(n):
-    if n <= 1:
-        return 0
-    count = 0
-    quaer = 1
-    while n > quaer:
-        count += 1
-        quaer *= 2
-    return count
-
-def movements_number(people_number):
-    number = 0
-    while people_number > 2 ** number:
-        number += 1
-    return number
-
-for i in range(100):
-    print(group(i), '-', movements_number(i), '----', i)
+print(4%4)
